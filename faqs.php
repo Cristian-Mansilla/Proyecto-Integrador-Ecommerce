0ca32443c2b502940421abf886617b9e1a13cc41
Cristian Mansilla <cristian-mansilla@live.com.ar>
2019-09-19 18:13:24 -0300

    footer.php agregado a faqs

--- faqs.php
+++ faqs.php
@@ -73,47 +73,7 @@
     </section>
     <!--finaliza sección preguntas frecuentes-->
     <!--inicia pie de página-->
-    <footer class="container-fluid justify-content-between row backg-footer">
-      <div class="col-lg-3 col-md-5">
-        <h3 class="col-12 text-white">Comprar y vender</h3>
-        <ul class="list-unstyled">
-          <li class="ml-3"><a class="text-white text-decoration-none" href="#">Registro</a> </li>
-          <li class="ml-3"><a class="text-white text-decoration-none" href="#">Marcas</a> </li>
-          <li class="ml-3"><a class="text-white text-decoration-none" href="#">Como comprar y vender</a> </li>
-          <li class="ml-3"><a class="text-white text-decoration-none" href="#">Productos populares</a> </li>
-        </ul>
-      </div>
-      <div class="col-lg-3 col-md-5">
-        <h3 class="text-white col-12">Metodos de pago</h3>
-        <ul class="list-unstyled ">
-          <li class="ml-3"><a class="text-white text-decoration-none" href="#">Metodos de pago</a></li>
-          <li class="ml-3"><a class="text-white text-decoration-none" href="#">Cupones especiales</a></li>
-          <li class="ml-3"><a class="text-white text-decoration-none" href="#">Promociones con tarjetas</a></li>
-        </ul>
-      </div>
-      <div class="col-lg-3 col-md-5 backg-footer">
-        <h3 class="text-white col-12">Mas sobre nosotros</h3>
-        <ul class="list-unstyled">
-          <li class="ml-3"><a class="text-white text-decoration-none" href="#">Acerca de nosotros</a></li>
-          <li class="ml-3"><a class="text-white text-decoration-none" href="#">Desarrolladores</a></li>
-          <li class="ml-3"><a class="text-white text-decoration-none" href="#">Politicas y reglas</a></li>
-
-        </ul>
-      </div>
-      <div class="col-lg-3 col-md-5 backg-footer">
-        <h3 class="text-white col-12">Ayuda</h3>
-        <ul class="list-unstyled">
-          <li class="ml-3"><a class="text-white text-decoration-none" href="#">Atencion al cliente</a></li>
-          <li class="ml-3"><a class="text-white text-decoration-none" href="#">Gestionar compras</a></li>
-          <li class="ml-3"><a class="text-white text-decoration-none" href="#">Contacto</a></li>
-          <li class="ml-3"><a class="text-white text-decoration-none" href="#">Reportar Problema</a></li>
-          <li class="ml-3"><a class="text-white text-decoration-none" href="#">Medios de pago</a></li>
-          <li class="ml-3"><a class="text-white text-decoration-none" href="#">Ayuda</a></li>
-
-        </ul>
-      </div>
-
-    </footer>
+    <?php require_once("footer.php") ?>
     <!--finaliza pie de página-->
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
