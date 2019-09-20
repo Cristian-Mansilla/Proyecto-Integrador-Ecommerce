b9532a19e72f65b65b52c0124dbfed331cb40a3b
Cristian Mansilla <cristian-mansilla@live.com.ar>
2019-09-19 22:35:21 -0300

    estilizando faqs

--- faqs.php
+++ faqs.php
@@ -34,7 +34,7 @@
     </section>
     <!--finaliza sección volver atrás-->
     <!--inicia sección preguntas frecuentes-->
-    <section class="bg-white">
+    <section class="bg-white pb-4 pt-4">
       <div class="container">
         <ul class="list-none">
           <li>
@@ -42,6 +42,17 @@
               <h2 class="first-letter">FAQS</h2>
             </div>
           </li>
+          <div class="flex-container">
+            <div class="w-25">
+              <hr>
+            </div>
+            <div>
+              <ion-icon name="ios-bowtie"></ion-icon>
+            </div>
+            <div class="w-25">
+              <hr>
+            </div>
+          </div>
           <ol class="hideAndSeek">
             <li>
               <a href="#a">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor vehicula ipsum nec ?</a>
@@ -76,6 +87,7 @@
     <?php require_once("footer.php") ?>
     <!--finaliza pie de página-->
 
+    <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
