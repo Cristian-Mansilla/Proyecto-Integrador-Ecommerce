4eb548c82b1037cb5006f834061efc91de2266e7
Nicolas Morero <nmorero00@gmail.com>
2019-09-24 13:50:50 -0300

    agregando contacto

---
+++ contacto.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Ecommerce</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Kanit:300,400&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/master.css">
+  </head>
+  <body>
+    <?php  require("header.php");  ?>
+
+    <div class="container mt-2 pt-4 mb-2">
+      <h2 class="text-center p-2" style="font-family:kanit;">Ponte en contacto con nosotros</h2>
+      <form action="registro.php" class="row pt-4" method="post">
+        <div class="form-group col-4 " >
+          <input class="form-control bg-dark text-white" style="height:60px;" type="text" name="name" value="" placeholder="Nombre">
+        </div>
+        <div class="form-group col-4" >
+          <input class="form-control bg-dark text-white" style="height:60px;" type="text" name="phone" value="" placeholder="Telefono">
+        </div>
+        <div class="form-group col-4" >
+          <input class="form-control bg-dark text-white" style="height:60px;" type="email" name="email" value="" placeholder="Correo electrónico">
+        </div>
+        <div class="form-group col-12">
+          <textarea class="form-control bg-dark text-white" name="name" rows="8"  placeholder="Mensaje"></textarea>
+        </div>
+
+        <div class="col-12 mt-4 d-flex justify-content-end">
+          <button type="submit" class="btn btn-primary col-4" name="enviar" value="Enviar">Enviar consulta</button>
+        </div>
+
+      </form>
+      <br><br><br><br><br><br><br><br><br><br>
+    </div>
+
+
+
+
+    <?php  require("footer.php");  ?>
+    <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+  </body>
+</html>
