ca181478099981db1534af26275339f5294f9b5d
Nicolas Morero <nmorero00@gmail.com>
2019-09-26 18:56:01 -0300

    Cambie a cards los ptoductos

--- agregarProd.php
+++ agregarProd.php
@@ -4,9 +4,11 @@
 $datos = file_get_contents("productos.json");
 $productos = json_decode($datos, true);
 
-
+$precios = "$ 200";
 $productos[] = [
-  "ruta" => "img/index/computadora.jpeg"
+  "ruta" => "img/index/computadora.jpeg",
+  "titulo" => "producto1",
+  "precio" => $precios
 ];
 
 $jsonFinal = json_encode($productos);
--- imprimirProds.php
+++ imprimirProds.php
@@ -10,7 +10,19 @@ $productos = json_decode($datos, true);
 
 foreach ($productos as $producto) {
   $ruta = $producto["ruta"];
-  echo "<a href='#top' class='col-lg-3 col-md-6 mt-md-6 col-sm-6 col-12'><img src='$ruta' class=' rounded-circle'></a>";
+  $titulo = $producto["titulo"];
+  $precio = $producto["precio"];
+
+
+  echo "<a href='#top' class='text-decoration-none'><div class='card' style='width: 18rem;'>
+          <img src='$ruta' class='card-img-top' alt='...'>
+          <div class='card-body'>
+            <h5 class='card-title'>$titulo</h5>
+            <p class='card-text'>$precio</p>
+          </div>
+      </div></a>";
+
+  // echo "<a href='#top' class='col-lg-3 col-md-6 mt-md-6 col-sm-6 col-12'><img src='$ruta' class=' rounded-circle'></a>";
 
 
 
