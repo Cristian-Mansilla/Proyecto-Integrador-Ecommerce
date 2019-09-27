5b8fd8e7b55bd7a485e5a625cf53f2a5a150089d
Nicolas Morero <nmorero00@gmail.com>
2019-09-26 21:21:30 -0300

    Agregar carpetas con imganes listo

--- validarImgRegistro.php
+++ validarImgRegistro.php
@@ -2,6 +2,10 @@
   if($_FILES["imgPerfil"]["error"] == 0){
     $nombre = $_FILES["imgPerfil"]["name"];
     $path = pathinfo($nombre, PATHINFO_EXTENSION);
+    $carpeta = "imgPerfiles/" . $_POST["email"];
+    if (!file_exists($carpeta)) {
+      mkdir($carpeta, 0777, true);
+    }
     if($path === "jpg" || $path === "jpeg" || $path == "png"){
       move_uploaded_file($_FILES["imgPerfil"]["tmp_name"], "imgPerfiles/" . $_POST["email"] . "/perfil." . $path);
     }
