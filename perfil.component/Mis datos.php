5138e9516624d7e246a94cb94222e4a412a58685
Cristian Mansilla <cristian-mansilla@live.com.ar>
2019-09-28 14:11:05 -0300

    hoja de estilo para Mis datos

--- perfil.component/Mis datos.php
+++ perfil.component/Mis datos.php
@@ -1,8 +1,10 @@
 <div class="">
   <h1>Mis Datos</h1>
-  <form class="" action="index.html" method="post">
-    <h2>Datos de cuenta</h2>
-    <input type="text" name="" value="" placeholder="Usuario">
-    <input type="email" name="" value="" placeholder="E-mail">
+  <form class="" action="" method="post">
+    <div class="form-group">
+      <h2>Datos de cuenta</h2>
+      <input class="form-control border-bottom-only" type="text" name="" value="" placeholder="Usuario">
+      <input class="form-control border-bottom-only" type="email" name="" value="" placeholder="E-mail">
+    </div>
   </form>
 </div>
