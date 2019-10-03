a63312a475405acd118e718a3b6787d44a9e9c7b
Cristian Mansilla <cristian-mansilla@live.com.ar>
2019-10-03 17:07:37 -0300

    algo de margen para los input de Mis datos

--- perfil.component/Mis datos.php
+++ perfil.component/Mis datos.php
@@ -4,13 +4,13 @@
   <form class="" action="" method="post">
     <div class="form-group">
       <h2>Datos de cuenta</h2>
-      <input class="form-control border-br" type="text" name="username" value="<?=$nombreUsuario?>" placeholder="Usuario">
+      <input class="form-control border-br mb-2" type="text" name="username" value="<?=$nombreUsuario?>" placeholder="Usuario">
       <button class="btn btn-secondary btn-block" type="button" name="changePassword">Cambiar contraseña</button>
     </div>
     <div class="form-group">
       <h2>Datos personales</h2>
-      <input class="form-control border-br" type="text" name="name" value="<?=$nombrePropio?>" placeholder="Nombre">
-      <input class="form-control border-br" type="text" name="lastname" value="<?=$apellido?>" placeholder="Apellido">
+      <input class="form-control border-br mb-2" type="text" name="name" value="<?=$nombrePropio?>" placeholder="Nombre">
+      <input class="form-control border-br mb-2" type="text" name="lastname" value="<?=$apellido?>" placeholder="Apellido">
       <input class="form-control border-br" type="email" name="email" value="<?=$correo?>" placeholder="E-mail">
     </div>
     <button class="btn btn-success mx-auto d-block inflate"  type="submit" name="guardar">Guardar</button>
