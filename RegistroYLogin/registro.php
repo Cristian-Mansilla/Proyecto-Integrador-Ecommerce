0538ab06644c52548c59617c114ad8d15a00cb87
uranmarcos <marcos_uran@hotmail.com>
2019-10-15 11:56:07 -0300

    Arreglo persistencia

--- RegistroYLogin/registro.php
+++ RegistroYLogin/registro.php
@@ -1,16 +1,31 @@
 <?php
   require_once ("validarRegistro.php");
 
+  $usuario = " ";
+  $nombre = " ";
+  $apellido = " ";
+  $email = " ";
+  $contrasena = " ";
+
+//persistencia
+  if($_POST){
+    $usuario = $_POST["username"];
+    $nombre = $_POST["name"];
+    $apellido = $_POST["lastname"];
+    $email = $_POST["email"];
+
+  }
+
   if($_POST) {
 
     $errores = validarRegistro();
 
-    // var_dump($errores);
+    var_dump($errores);
 
     if (empty($errores)) {
       // llamo al archivo json usuarios
       require_once("validarImgRegistro.php");
-      $datos = file_get_contents("../usuarios.json");
+      $datos = file_get_contents("usuarios.json");
       $usuarios = json_decode($datos, true);
       // determino los valores de los post del formulario
       $usuario = $_POST["username"];
@@ -29,11 +44,12 @@
       ];
       // vuelvo a codificar y enviar los datos al archivo json
       $jsonFinal = json_encode($usuarios);
-      file_put_contents("../usuarios.json", $jsonFinal);
+      file_put_contents("usuarios.json", $jsonFinal);
       // Envio al index si el registro esta completado
-      header("location:../index.php");
+      header("location:index.php");
     }
   }
+
 ?>
 
 <!DOCTYPE html>
@@ -43,7 +59,7 @@
     <title>Ecommerce | Registro</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/master.css">
+    <link rel="stylesheet" href="css/master.css">
   </head>
   <body class="body-register">
     <div class="container">
@@ -58,16 +74,16 @@
             </ul>
           <?php endif; ?>
           <div class="form-group">
-            <input class="form-control" type="text" name="username" value="" placeholder="Usuario">
+            <input class="form-control" type="text" name="username" value="<?= $usuario ?>" placeholder="Usuario">
           </div>
           <div class="form-group">
-            <input class="form-control" type="text" name="name" value="" placeholder="Nombre">
+            <input class="form-control" type="text" name="name" value="<?= $nombre ?>" placeholder="Nombre">
           </div>
           <div class="form-group">
-            <input class="form-control" type="text" name="lastname" value="" placeholder="Apellido">
+            <input class="form-control" type="text" name="lastname" value="<?= $apellido ?>" placeholder="Apellido">
           </div>
           <div class="form-group">
-            <input class="form-control" type="email" name="email" value="" placeholder="Correo electrónico">
+            <input class="form-control" type="email" name="email" value=" <?= $email ?>" placeholder="Correo electrónico">
           </div>
           <div class="form-group">
             <input class="form-control" type="password" name="password" value="" placeholder="Contraseña">
@@ -82,7 +98,7 @@
             <br>
             <p>Al completar este registro declara ser mayor de 18 años y acepta nuestros <a href="#">términos y cóndiciones</a></p>
             <br>
-            <p>¿Ya tienes una cuenta? <a href="login.php">Iniciá sesión</a></p>
+            <p>¿Ya tienes una cuenta? <a href="login.html">Iniciá sesión</a></p>
             </div>
 
 
